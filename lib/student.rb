2898a60367755d0199fd1525d66a486b68f13f5d
Lizzie Chan <lchan217@gmail.com>
2019-03-26 17:41:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,16 +41,16 @@ class Student
       self.new_from_db(row)
     end
   end 
-  def self.students_below_12th_grade
-    sql = <<-SQL 
-      SELECT * 
-      FROM students
-      WHERE grade < ?
-    SQL
-    DB[:conn], execute(sql,12).map do |row|
-      self.new_from_db(row)
-     
-  end 
+  # def self.students_below_12th_grade
+  #   sql = <<-SQL 
+  #     SELECT * 
+  #     FROM students
+  #     WHERE grade < ?
+  #   SQL
+  #   DB[:conn], execute(sql,12).map do |row|
+  #     self.new_from_db(row)
+  #   end 
+  # end 
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
