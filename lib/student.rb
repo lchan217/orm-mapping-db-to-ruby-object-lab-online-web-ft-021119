b4278be1dee1af160ec91c55393b0e1fb34851a6
Lizzie Chan <lchan217@gmail.com>
2019-03-26 17:33:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,10 +11,10 @@ class Student
   end
 
   def self.all
-    sql = <<-SQL
-      SELECT * 
-      FROM students 
-    SQL 
+    sql = <<-SQL 
+      SELECT *
+      FROM students
+    SQL
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
   end
