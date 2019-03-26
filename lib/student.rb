02022f3f6c612534d427759d1eef9e65d177f6fd
Lizzie Chan <lchan217@gmail.com>
2019-03-26 17:38:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,7 +37,7 @@ class Student
       FROM students
       WHERE grade = ? 
     SQL
-    db[:conn].execute(sql,9).map do |row|
+    DB[:conn].execute(sql,9).map do |row|
       self.new_from_db(row)
     end
   end 
